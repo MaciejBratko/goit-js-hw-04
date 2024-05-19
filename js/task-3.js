65cd948dfe6245529d183c567fdb07f798fe05cf
MaciejBratko <maciej.bratko@outlook.com>
2024-05-19 21:14:09 +0200

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,15 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
-  getInfo: function () {},
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
